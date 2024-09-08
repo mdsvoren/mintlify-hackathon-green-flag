47b4729370920dd2ee20284417922d061b006c53
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-09-07 22:41:27 -0700

    start function for checking repo

--- index.ts
+++ index.ts
@@ -2,15 +2,26 @@ import { env } from "./env.js";
 import { App } from "octokit";
 import express from "express";
 
-const app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.listen(3000);
-
 const githubApp = new App({
   appId: env.GITHUB_APP_ID,
   privateKey: env.GITHUB_PRIVATE_KEY,
 });
+
+const app = express();
+
+app.post('/check/:owner/:repo', async function (req, res) {
+  try {
+    const { owner, repo } = req.params;
+    const { data: { id } } = await githubApp.octokit.request('GET /repos/{owner}/{repo}/installation', {
+      owner,
+      repo,
+    })
+    console.log(`repo ${owner}/${repo} has installation id ${id}`);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send();
+  }
+  return res.status(200).send();
+})
+
+app.listen(3000);
