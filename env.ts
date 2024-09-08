348170c50a1d102f62937ae7fae8bfc5d59f9c39
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-09-07 22:02:07 -0700

    delete webhook code

--- env.ts
+++ env.ts
@@ -4,10 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = cleanEnv(process.env, {
-  WEBHOOK_SECRET: str(),
   GITHUB_APP_ID: str(),
   GITHUB_PRIVATE_KEY: str(),
-  SMEE_URL: str(),
   GREPTILE_API_KEY: str(),
   GITHUB_TOKEN: str(),
   ANTHROPIC_API_KEY: str(),
