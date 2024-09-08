3ed7aff7fa438f55d0a8d2bbd1121b6d120dcd62
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-09-07 23:12:06 -0700

    create getBlob fn

--- github-app.ts
+++ github-app.ts
@@ -1,7 +1,32 @@
-import { App } from "octokit";
+import { App, Octokit } from "octokit";
 import { env } from "./env.js";
 
 export const githubApp = new App({
   appId: env.GITHUB_APP_ID,
   privateKey: env.GITHUB_PRIVATE_KEY,
 });
+
+export async function getOctokit(owner: string, repo: string) {
+  const { data: { id } } = await githubApp.octokit.request('GET /repos/{owner}/{repo}/installation', {
+    owner,
+    repo,
+  });
+  return await githubApp.getInstallationOctokit(id);
+}
+
+export async function getBlob(octokit: Octokit, owner: string, repo: string, path: string) {
+  const { data } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+    owner,
+    repo,
+    path,
+  });
+
+  if (Array.isArray(data)) {
+    throw new Error('Path is a directory');
+  }
+  if (data.type !== 'file') {
+    throw new Error('Path is not a file');
+  }
+
+  return Buffer.from(data.content, 'base64').toString('utf-8');
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { githubApp } from "./github-app.js";
+import { getOctokit, githubApp } from "./github-app.js";
 import { Octokit } from 'octokit';
 import { env } from "./env.js";
 import { ClaudeSonnetClient } from "./anthropic-client.js";
@@ -9,11 +9,7 @@ const app = express();
 app.post('/check/:owner/:repo', async function (req, res) {
   try {
     const { owner, repo } = req.params;
-    const { data: { id } } = await githubApp.octokit.request('GET /repos/{owner}/{repo}/installation', {
-      owner,
-      repo,
-    })
-    console.log(`repo ${owner}/${repo} has installation id ${id}`);
+    const octokit = await getOctokit(owner, repo);
   } catch (e) {
     console.error(e);
     return res.status(500).send();
