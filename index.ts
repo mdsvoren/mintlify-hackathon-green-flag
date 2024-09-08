ab4b344c45de1a42ef8e7ec2bb87f45ef88a57c9
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-09-07 22:10:32 -0700

    add express app

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
-import { Webhooks, createNodeMiddleware } from "@octokit/webhooks";
 import { env } from "./env.js";
 import { App } from "octokit";
+import express from "express";
 
-const app = new App({
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000);
+
+const githubApp = new App({
   appId: env.GITHUB_APP_ID,
   privateKey: env.GITHUB_PRIVATE_KEY,
 });
